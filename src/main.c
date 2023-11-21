0cb387568b0512fecb7634c8d828a996574c028e
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-11-20 23:27:56 -0500

    Refactor formatter code guide style

--- src/main.c
+++ src/main.c
@@ -1,22 +1,20 @@
-#include "stdio.h"
 #include "my_lib.h"
+#include "stdio.h"
 
 int main() {
-    int first = 3;
-    int second = 1;
+  int first = 3;
+  int second = 1;
 
-    int value = first + second;
-    printf("The sum of %d and %d is %d\n", first, second, value);
+  int value = first + second;
+  printf("The sum of %d and %d is %d\n", first, second, value);
 
-    if(value <= 3) {
-        printf("The sum is less than or equal to 3\n");
-    } else {
-        printf("The sum is greater than 3\n");
-    }
-    greet();
-    return 0;
+  if (value <= 3) {
+    printf("The sum is less than or equal to 3\n");
+  } else {
+    printf("The sum is greater than 3\n");
+  }
+  greet();
+  return 0;
 }
 
-void greet() {
-    printf("Hello World!\n");
-}
+void greet() { printf("Hello World!\n"); }
