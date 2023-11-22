3534548a768b70cd62e354ec91b3117c60a96e4a
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-11-22 02:25:24 +0000

    Add types float

--- src/main.c
+++ src/main.c
@@ -1,12 +1,21 @@
-#include "my_lib.h"
 #include "stdio.h"
+#include "my_lib.h"
 
+/**
+  * This is a simple program that adds two numbers and prints the result.
+  * It also prints a message depending on the result.
+  
+*/
 int main() {
+  // This is a comment
   int first = 3;
   int second = 1;
-
+  float third = 3.0;
+  double fourth = 3.0;
   int value = first + second;
+
   printf("The sum of %d and %d is %d\n", first, second, value);
+  printf("float: %d", third + fourth);
 
   if (value <= 3) {
     printf("The sum is less than or equal to 3\n");
@@ -14,6 +23,8 @@ int main() {
     printf("The sum is greater than 3\n");
   }
   greet();
+
+  printf("The sum of: %d", 3 + 3);
   return 0;
 }
 
